c66608ff468f6a71969ce09d6656f4ea6f5d1fc4
Marco <marcoerhardt95@gmail.com>
2021-02-18 09:55:13 +0100

    refactor application logic in posts partial

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -4,4 +4,14 @@ module PostHelper
 
     content_tag :p, "Post could not be saved. #{post.errors.full_messages.join('. ')}", class: 'errors'
   end
+
+  def find_friendship(friendship, user_id, friend_id, confirmed)
+    friendship.find_by(user_id: user_id, friend_id: friend_id, confirmed: true)
+  end
+
+  def show_timeline
+    if find_friendship(@friendship, current_user, post.user, true) or current_user == post.user
+      render @timeline_posts
+    end
+  end
 end
